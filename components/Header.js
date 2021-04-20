55fd7289cdc8935099c9d9e04c9e0499ff5390e5
amine kammoun <aminekammoun55@gmail.com>
2021-04-20 17:38:58 +0100

    first commit

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+    StatusBar,
+    FlatList,
+    Image,
+    Animated,
+    Text,
+    View,
+    Dimensions,
+    StyleSheet,
+    TouchableOpacity,
+    Easing,
+    SafeAreaViewBase,
+    SafeAreaView,
+  } from "react-native";
+
+export default function Header() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
